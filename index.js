a97f6843ae152a77497b2077e8b3c5e03b659e7f
Necoline <hnhubner@gmail.com>
2019-05-07 23:07:03 -0700

    updates class names

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-//class name change
-// comment functions
-// <
-
-
 const carousel = (function () {
 
     function animateSlide() {
-        const slides = document.querySelectorAll('.carousel .slide');
+        const slides = document.querySelectorAll('.Carousel__wrapper .Carousel__slide');
         let isEnabled = true;    
         let currentSlide = 0;
     
@@ -15,28 +10,26 @@ const carousel = (function () {
         }
         
         function nextSlide(n) {
-            hideSlide('to-left');
+            hideSlide('Carousel__to-left');
             changeCurrentSlide(n + 1);
-            showSlide('from-right');
+            showSlide('Carousel__from-right');
         }
         
-
-
         function previousSlide(n) {
-            hideSlide('to-right');
+            hideSlide('Carousel__to-right');
             changeCurrentSlide(n - 1);
-            showSlide('from-left');
+            showSlide('Carousel__from-left');
         }
         
         function goToSlide(n) {
             if (n < currentSlide) {
-                hideSlide('to-right');
+                hideSlide('Carousel__to-right');
                 currentSlide = n;
-                showSlide('from-left');
+                showSlide('Carousel__from-left');
             } else {
-                hideSlide('to-left');
+                hideSlide('Carousel__to-left');
                 currentSlide = n;
-                showSlide('from-right');
+                showSlide('Carousel__from-right');
             }
         }
         
@@ -44,40 +37,46 @@ const carousel = (function () {
             isEnabled = false;
             slides[currentSlide].classList.add(direction);
             slides[currentSlide].addEventListener('animationend', function() {
-                this.classList.remove('active', direction);
+                this.classList.remove('Carousel__active', direction);
             });
         }
         
         function showSlide(direction) {
-            slides[currentSlide].classList.add('next', direction);
+            slides[currentSlide].classList.add('Carousel__next', direction);
             slides[currentSlide].addEventListener('animationend', function() {
-                this.classList.remove('next', direction);
-                this.classList.add('active');
+                this.classList.remove('Carousel__next', direction);
+                this.classList.add('Carousel__active');
                 isEnabled = true;
             });
         }
     
-        document.querySelector('.carousel-control.left').addEventListener('click', function() {
+        document.querySelector('.Carousel__control.left').addEventListener('click', function() {
             if (isEnabled) {
                 previousSlide(currentSlide);
             }
         });
         
-        document.querySelector('.carousel-control.right').addEventListener('click', function() {
+        document.querySelector('.Carousel__control.right').addEventListener('click', function() {
             if (isEnabled) {
                 nextSlide(currentSlide);
             }
         });
     }
     
-    function create(imageArray) {
-        const anchor = document.querySelector('[data-carousel-anchor]')
-        const result = imageArray.reduce((accum, currentImage) =>
-            accum.concat(`<div class="slide"><div class="container"><img src="${currentImage}" /></div></div>`), '')
-        
-        anchor.insertAdjacentHTML('beforeend', `<div class="carousel"><div class="carousel-inner">${result}</div><div class="carousel-control left"><div class="arrow left"></div></div><div class="carousel-control right"><div class="arrow right"></div></div></div>` )
+    function create(imageProps) {
+        const { anchorName, images } = imageProps
+        const anchor = document.querySelector(`[data-carousel-anchor=${anchorName}]`)
 
-        document.getElementsByClassName('slide')[0].className = 'slide active'
+        if (!anchor) {
+            throw new Error('Anchor name does not match anchor tag in html')
+        }
+
+        const image = images.reduce((accum, currentImage) =>
+            accum.concat(`<div class="Carousel__slide"><div class="Carousel__container"><img src="${currentImage}" /></div></div>`), '')
+        
+        anchor.insertAdjacentHTML('beforeend', `<div class="Carousel__wrapper"><div class="Carousel__inner">${image}</div><div class="Carousel__control left"><div class="Carousel__arrow left"></div></div><div class="Carousel__control right"><div class="Carousel__arrow right"></div></div></div>` )
+
+        document.getElementsByClassName('Carousel__slide')[0].className = 'Carousel__slide Carousel__active'
         
         animateSlide()
         
